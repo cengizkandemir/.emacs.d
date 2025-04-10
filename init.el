c6d80df8096b18098772e7d9790c9d2c59e0447c
Cengiz Kandemir <ckandemi@qti.qualcomm.com>
2025-04-10 14:22:37 +0200

    Split ediff window horizontally

--- init.el
+++ init.el
@@ -153,6 +153,10 @@
   ((org-mode . turn-on-auto-fill)
    (org-mode . (lambda () (set-fill-column 120)))))
 
+(custom-set-variables
+ '(ediff-window-setup-function 'ediff-setup-windows-plain) ;; is this needed?
+ '(ediff-diff-options "-w")
+ '(ediff-split-window-function 'split-window-horizontally))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;; coding style ;;;;;;;;;;;;;;;;
